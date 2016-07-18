4eed86e13271e615e24fb4dd86ecaf3171572ee8
hasimandimby <rhasimandimby@gmail.com>
2016-07-18 23:15:48 +0300

    first commit

--- src/MIRE/AdminBundle/MIREAdminBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace MIRE\AdminBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class MIREAdminBundle extends Bundle
-{
-}
--- src/MIRE/FrontBundle/MIREFrontBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace MIRE\FrontBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class MIREFrontBundle extends Bundle
-{
-}
