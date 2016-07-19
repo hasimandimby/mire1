268f276dbc129dec37bc1d2b80a35d72aa6eae21
hasimandimby <rhasimandimby@gmail.com>
2016-07-19 20:16:12 +0300

    modification

---
+++ src/MIRE/AdminBundle/Form/ArticleType.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace MIRE\AdminBundle\Form;
+use  MIRE\AdminBundle\Form\ImageType;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+
+class ArticleType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+
+        $builder
+            ->add('date',DateType::class)
+            ->add('image_id',ImageType::class)
+            ->add('titre',TextType::class)
+            ->add('contenu',TextareaType::class)
+            ->add('id_auteur',IntegerType::class)
+            ->add('published',CheckboxType::class)
+            ->add('motcle', TextType::class)
+            ->add('save',SubmitType::class, array('label' => 'Modifier un article'))
+
+           // ->add('categories')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'MIRE\AdminBundle\Entity\Article'
+        ));
+    }
+}
