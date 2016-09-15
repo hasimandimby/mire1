9d7601d8fbf989325e6524f8561ac094dd87140e
hasimandimby <rhasimandimby@gmail.com>
2016-09-15 15:48:14 +0200

    niova toerana

---
+++ src/MIRE/AdminBundle/Entity/Service.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace MIRE\AdminBundle\Entity;
+
+/**
+ * Service
+ */
+class Service
+{
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $nom;
+
+    /**
+     * @var string
+     */
+    private $image;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nom
+     *
+     * @param string $nom
+     *
+     * @return Service
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    /**
+     * Get nom
+     *
+     * @return string
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * Set image
+     *
+     * @param string $image
+     *
+     * @return Service
+     */
+    public function setImage($image)
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    /**
+     * Get image
+     *
+     * @return string
+     */
+    public function getImage()
+    {
+        return $this->image;
+    }
+}
+
